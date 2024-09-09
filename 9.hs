adea378007a77e82b1f8fd4ce58787b4a1a12535
Valentín Coletta <81708912+valentincoletta322@users.noreply.github.com>
2024-09-09 13:59:14 -0300

    Add files via upload

--- 9.hs
+++ 9.hs
@@ -7,10 +7,10 @@ parteEntera :: Float -> Integer
 parteEntera x | x < 1 = 0
               | otherwise = 1 + parteEntera (x-1)
 
-parteEnteraN :: Float -> Integer
-parteEnteraN x | (x < 1) && (x > (-1)) = 0
-               | x <= (-1) = 1 + parteEnteraN (x+1)
-               | x >= 1 = 1 + parteEnteraN (x-1)
+parteEnteraNeg :: Float -> Integer
+parteEnteraNeg x | (x < 1) && (x > (-1)) = 0
+                 | x <= (-1) = (-1) + parteEnteraNeg (x+1)
+                 | x >= 1 = 1 + parteEnteraNeg (x-1)
                
 iesimoDigito :: Integer -> Integer -> Integer
 iesimoDigito n i = div (mod (abs n) (10^(exp))) (10^(exp-1))
@@ -23,5 +23,8 @@ cantDigitos n | n <= 9 && n >= 0 = 1
 esCapicua :: Integer -> Bool
 esCapicua n | ultimo /= primero = False
             | ultimo == primero && cantDigitos n <= 2 = True
-            | otherwise = esCapicua (mod (div n 10) (10^((cantDigitos n)-2)))
+            | otherwise = esCapicua (sacarBordes n)
             where (ultimo, primero) = ((iesimoDigito n (cantDigitos n)), (iesimoDigito n 1))
+
+sacarBordes :: Integer -> Integer
+sacarBordes n = mod (div n 10) (10^((cantDigitos n)-2))
